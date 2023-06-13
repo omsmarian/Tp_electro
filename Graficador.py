072441f47550b1ead048ca5c318525671ca84ee5
Mariano Oms <moms@itba.edu.ar>
2023-06-13 02:09:35 -0300

    Ya hay update y el front casi terminado

--- Graficador.py
+++ Graficador.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from backend import filtro
+#from backend import filtro
 
 import numpy as np
 from scipy import signal
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 
 class Ui_Ploter(object):
 
-    meme = filtro()
+    #meme = filtro()
 
     def setupUi(self, Ploter):
         Ploter.setObjectName("Ploter")
@@ -522,22 +522,7 @@ class Ui_Ploter(object):
         self.unitampent.setItemText(1, _translate("Ploter", "KV"))
         self.unitampent.setItemText(2, _translate("Ploter", "MV"))
 
-        self.filtro1.activated.connect(lambda: self.lineeditcheck(self.filtro1))
-        self.filtro2.activated.connect(lambda: self.lineeditcheck(self.filtro2))
-        #primer orden
-        self.fo1.editingFinished.connect(lambda: self.getnum(self.fo1))  
-        self.fp1.editingFinished.connect(lambda: self.getnum(self.fp1))  
-        self.ganancia1.editingFinished.connect(lambda: self.getnum(self.ganancia1))
-        self.bandapa1.toggled.connect(lambda: self.plot())
-        #segundo orden
-        self.fo2.editingFinished.connect(lambda: self.getnum(self.fo2))
-        self.fp2.editingFinished.connect(lambda: self.getnum(self.fp2))
-        self.psio.editingFinished.connect(lambda: self.getnum(self.psio))
-        self.psip.editingFinished.connect(lambda: self.getnum(self.psip))
-        self.ganancia2.editingFinished.connect(lambda: self.getnum(self.ganancia2))
-        #orden sup
-        self.numerador.editingFinished.connect(lambda: self.getnum(self.numerador))
-        self.denominador.editingFinished.connect(lambda: self.getnum(self.denominador))
+        self.update()      #para q sea mas legible el código
         
     
     #extrae el número ingresado
@@ -562,77 +547,180 @@ class Ui_Ploter(object):
     
     #deja escribir o no en base al index
     def lineeditcheck(self, combobox):
-        if combobox.currentIndex() == 3 or combobox.currentIndex() == 5 or combobox.currentIndex() == 6 or combobox.currentIndex() == 7:
+        ifcondition = (combobox.currentIndex() == 3 or combobox.currentIndex() == 5 or 
+            combobox.currentIndex() == 6 or combobox.currentIndex() == 7)
+        self.clearall()
+        if ifcondition:
             self.fo1.setReadOnly(False)
             self.fo2.setReadOnly(False)
             self.psio.setReadOnly(False)
-            self.fp2.clear()
             self.fp2.setReadOnly(False)
             self.changename(True)
             if combobox.currentIndex() == 3:
-                self.fp2.clear()
                 self.fp2.setReadOnly(True)
+                self.psip.setReadOnly(True)
                 self.changename(False)
             elif combobox.currentIndex() == 7:
                 self.changename(False)
         else:
             self.changename(False)
             self.psio.setReadOnly(True)
-            self.psio.clear()
             self.fo1.setReadOnly(True)
-            self.fo1.clear()
             self.fo2.setReadOnly(True)
-            self.fo2.clear()
             self.fp2.setReadOnly(False)
+            self.psip.setReadOnly(False)
 
-    def update(self, flag):
-        return
+    #recibe flag y guarda todos los datos
+    def updateparams(self, orden):
+        #flags 1, primer orden, 2 segundo, 3 superior
+        if orden == 1:
+            fo = self.getnum(self.fo1) * self.get_multiplier(self.unitfo1)
+            fp = self.getnum(self.fp1) * self.get_multiplier(self.unitfp1)      #Eze hace lo q quieras acá lo puse asi para tenerlo
+            ganancia = self.getnum(self.ganancia1)          
+            gananciatype = self.getganancia(orden)
+            filtertype = self.getindex(self.filtro1)
+        elif orden == 2:
+            fo = self.getnum(self.fo2) * self.get_multiplier(self.unitfo2)
+            fp = self.getnum(self.fp2) * self.get_multiplier(self.unitfp2)      #Eze acá tmb
+            ganancia = self.getnum(self.ganancia2)          
+            gananciatype = self.getganancia(orden)
+            filtertype = self.getindex(self.filtro2)
+        #else: #filtro de tercer orden
+            #NO TOCAMOS ACÁ TODAVIA PQ ME VA A DAR ALGO
+            #es un update no deberia returnear nada,
+            # calculo q solo sube los datos a la clase filter
+    
+    #returns multiplier
+    def get_multiplier(self, combobox):
+        comboboxindex = combobox.currentIndex()
+        if comboboxindex == 0:
+            multiplier = 1
+        elif comboboxindex == 1:
+            multiplier = 1000
+        elif comboboxindex == 2:
+            multiplier = 1e6
+        else:
+            multiplier = 1e9
+        return multiplier
 
-    def plot (self):
-         
-        fo = 100/(2*np.pi)
-        self.meme.set_SO('PASO', fo, 0.5)
-
-        self.axes_module.clear()
-        self.axes_fase.clear()
-        self.axes_entrada.clear()
-        self.axes_cerospolos.clear()
-
-        # Module
-        self.axes_module.semilogx(self.meme.w, self.meme.Hdb)
-        self.axes_module.set_xlabel('σ')
-        self.axes_module.set_ylabel('jω')
-        self.axes_module.grid()
-        self.bodemodplt.set_tight_layout('True')
-        self.canvas1.draw()
+    #returns ganancia type
+    def getganancia(self, ordenflag):
+        if ordenflag == 1:
+            # 1 banda pasante, 2 maxima
+            if self.bandapa1.isChecked():
+                gananciatype = 1
+            else:
+                gananciatype = 2
+        else:
+            # 1 banda pasante, 2 maxima
+            if self.bandapa2.isChecked():
+                gananciatype = 1
+            else:
+                gananciatype = 2
+        return gananciatype
+    
+    # Funcion *aesthetic* solo para no tener un bloque gigante
+    # corre el update si tocas algo de la funcion transferencia
+    def checkforparamupdates(self):
+        #primer orden
+        self.filtro1.activated.connect(lambda: self.lineeditcheck(self.filtro1))
+        self.fo1.editingFinished.connect(lambda: self.updateparams(1))  
+        self.fp1.editingFinished.connect(lambda: self.updateparams(1))  
+        self.ganancia1.editingFinished.connect(lambda: self.updateparams(1))
+        self.bandapa1.toggled.connect(lambda: self.updateparams(1))
+        self.max1.toggled.connect(lambda: self.updateparams(1))
+        self.unitfo1.activated.connect(lambda: self.updateparams(1))
+        self.unitfp1.activated.connect(lambda: self.updateparams(1))
+        #segundo orden
+        self.filtro2.activated.connect(lambda: self.lineeditcheck(self.filtro2))
+        self.fo2.editingFinished.connect(lambda: self.updateparams(2))
+        self.fp2.editingFinished.connect(lambda: self.updateparams(2))
+        self.psio.editingFinished.connect(lambda: self.updateparams(2))
+        self.psip.editingFinished.connect(lambda: self.updateparams(2))
+        self.ganancia2.editingFinished.connect(lambda: self.updateparams(2))
+        self.bandapa2.toggled.connect(lambda: self.updateparams(2))
+        self.max2.toggled.connect(lambda: self.updateparams(2))
+        self.unitfo2.activated.connect(lambda: self.updateparams(2))
+        self.unitfp2.activated.connect(lambda: self.updateparams(2))
+        #orden sup
+        self.numerador.editingFinished.connect(lambda: self.updateparams(3))
+        self.denominador.editingFinished.connect(lambda: self.updateparams(3))
+    
+    #limpia todas las cajas de texto
+    def clearall(self):
+        self.fo1.clear()
+        self.fp1.clear()
+        self.fo2.clear()
+        self.fp2.clear()
+        self.psio.clear()
+        self.psip.clear()
+        self.ganancia1.clear()
+        self.ganancia2.clear()
+    
+    #updatea los datos de entrada
+    def updateinput(self):
+        frec = self.getnum(self.frecent) * self.get_multiplier(self.unitfrecent)
+        amp = self.getnum(self.ampent) * self.get_multiplier(self.unitampent)
+        entradatype = self.getindex(self.entradabox)
+        # acá lo subimos a filter tmb
+    
+    #chequea si se modifico algo de la entrada
+    def checkforinputupdates(self):
+        self.entradabox.activated.connect(lambda: self.updateinput())
+        self.frecent.editingFinished.connect(lambda: self.updateinput())
+        self.ampent.editingFinished.connect(lambda: self.updateinput())
+    
+    #update *aesthetic* para q se vea lindo
+    def update(self):
+        self.checkforinputupdates()
+        self.checkforparamupdates()
         
-        # Phase
-        self.axes_fase.semilogx(self.meme.w, self.meme.phi)
-        self.axes_fase.set_xlabel('σ')
-        self.axes_fase.set_ylabel('jω')
-        self.axes_fase.grid(True)
-        self.bodefaseplt.set_tight_layout('True')
-        self.canvas2.draw()
         
-        # Poles & zeros
-        self.axes_cerospolos.scatter(np.real(self.meme.zeros), np.imag(self.meme.zeros), marker = 'o', color = 'blue', label = 'Ceros')
-        self.axes_cerospolos.scatter(np.real(self.meme.poles), np.imag(self.meme.poles), marker = 'x', color = 'red', label = 'Polos')
-        self.axes_cerospolos.set_xlabel('σ')
-        self.axes_cerospolos.set_ylabel('jω')
-        self.axes_cerospolos.axhline(0, color='black', linewidth=1)
-        self.axes_cerospolos.axvline(0, color='black', linewidth=1)
-        self.axes_cerospolos.grid(True)
-        self.cerospolosplt.set_tight_layout('True')
-        self.canvas4.draw()
-
+    #def plot (self):
+    #     
+    #    fo = 100/(2*np.pi)
+    #    self.meme.set_SO('PASO', fo, 0.5)
+#
+    #    self.axes_module.clear()
+    #    self.axes_fase.clear()
+    #    self.axes_entrada.clear()
+    #    self.axes_cerospolos.clear()
+#
+    #    # Module
+    #    self.axes_module.semilogx(self.meme.w, self.meme.Hdb)
+    #    self.axes_module.set_xlabel('σ')
+    #    self.axes_module.set_ylabel('jω')
+    #    self.axes_module.grid()
+    #    self.bodemodplt.set_tight_layout('True')
+    #    self.canvas1.draw()
+    #    
+    #    # Phase
+    #    self.axes_fase.semilogx(self.meme.w, self.meme.phi)
+    #    self.axes_fase.set_xlabel('σ')
+    #    self.axes_fase.set_ylabel('jω')
+    #    self.axes_fase.grid(True)
+    #    self.bodefaseplt.set_tight_layout('True')
+    #    self.canvas2.draw()
+    #    
+    #    # Poles & zeros
+    #    self.axes_cerospolos.scatter(np.real(self.meme.zeros), np.imag(self.meme.zeros), marker = 'o', color = 'blue', label = 'Ceros')
+    #    self.axes_cerospolos.scatter(np.real(self.meme.poles), np.imag(self.meme.poles), marker = 'x', color = 'red', label = 'Polos')
+    #    self.axes_cerospolos.set_xlabel('σ')
+    #    self.axes_cerospolos.set_ylabel('jω')
+    #    self.axes_cerospolos.axhline(0, color='black', linewidth=1)
+    #    self.axes_cerospolos.axvline(0, color='black', linewidth=1)
+    #    self.axes_cerospolos.grid(True)
+    #    self.cerospolosplt.set_tight_layout('True')
+    #    self.canvas4.draw()
+#
         # Out        
-        self.axes_entrada.plot(meme.t, meme.Vin, label='Entrada', color='red')
-        self.axes_entrada.plot(meme.t, meme.Vout, label='Salida', color='blue')
-        self.axes_entrada.set_xlabel('σ')
-        self.axes_entrada.set_ylabel('jω')
-        self.axes_entrada.grid(True)
-        self.entradaplt.set_tight_layout('True')
-        self.canvas3.draw()
+        #self.axes_entrada.plot(meme.t, meme.Vin, label='Entrada', color='red')
+        #self.axes_entrada.plot(meme.t, meme.Vout, label='Salida', color='blue')
+        #self.axes_entrada.set_xlabel('σ')
+        #self.axes_entrada.set_ylabel('jω')
+        #self.axes_entrada.grid(True)
+        #self.entradaplt.set_tight_layout('True')
+        #self.canvas3.draw()
             
 
 if __name__ == "__main__":
